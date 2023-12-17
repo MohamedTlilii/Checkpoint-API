1d20e2b1d3e42ed73d4e41cc19fe74307d18bd4c
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-17 14:30:59 +0100

    almost-done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home"> 
+      <h1>HOME PAGE</h1>
+    </div>
   )
 }
 
--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
+import axios from "axios";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts") // Assuming you want users data, not posts
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>Posts</div>
   )
