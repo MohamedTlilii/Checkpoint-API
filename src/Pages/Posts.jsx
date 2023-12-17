479eb990cd15f74ee236a54c2d5c5bd056239e4b
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-17 15:07:53 +0100

    almost-done

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import axios from "axios";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts") // Assuming you want users data, not posts
+      .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => setPosts(response.data))
       .catch((error) => console.error(error));
   }, []);
+
   return (
-    <div>Posts</div>
-  )
+    <div className="posts">
+      {posts.map((posts) => (
+        <Card className="post" key={posts.id}>
+         
+          <Card.Content>
+            <Card.Header>{posts.title}</Card.Header>
+            <Card.Description>{posts.body}</Card.Description>
+          </Card.Content>
+          
+        </Card>
+      ))}
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
