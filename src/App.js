b507483302b6c69df36d3801b1dc2e7479830a57
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-17 13:57:46 +0100

    almost-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import NavBar from './Components/NavBar';
+import Home from './Pages/Home';
+import Posts from './Pages/Posts';
+import Users from './Pages/Users';
+import {Routes , Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-    
-    </div>
+      <NavBar/>
+     <Routes>
+      <Route path ="/" element={ <Home/>}/>
+      <Route path ="/users" element={ <Users/>}/>
+      <Route path ="/posts" element={ <Posts/>}/>
+     </Routes>
+        </div>
   );
 }
 
